a4eb3ff5e1d088705cff097fafb2bad6ebeaecf7
Kevin Pruett <pruett.kevin@gmail.com>
2014-10-15 23:22:27 -0400

    a bit more structure

--- config.rb
+++ config.rb
@@ -25,7 +25,7 @@ activate :blog do |blog|
   blog.sources = "articles/{title}.html"
   blog.permalink = "{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  blog.layout = "test"
+  blog.layout = "articles"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
